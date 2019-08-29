be94eb6aef47af65b332c444785cd1c4d94234a5
WoroodDabbas <30387117+WoroodDabbas@users.noreply.github.com>
2019-08-29 04:17:01 +0400

    Update app.js

--- 02a-simple-node-web-app/app.js
+++ 02a-simple-node-web-app/app.js
@@ -11,59 +11,30 @@ const app = express();
 // and is meant for debugging and developing.
 // For a list of stores, see compatible session stores below
 // https://www.npmjs.com/package/express-session#compatible-session-stores
-app.use(session({
-	secret: '123456',
-	resave: true,
-	saveUninitialized: true
+// app.use(session({
+// 	secret: '123456',
+// 	resave: true,
+// 	saveUninitialized: true
 }));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.serializeUser((user, cb) => cb(null, user));
-passport.deserializeUser((user, cb) => cb(null, user));
-passport.use(new WebAppStrategy({
-	tenantId: "",
-	clientId: "",
-	secret: "",
-	oauthServerUrl: "",
-	redirectUri: "http://localhost:3000/appid/callback"
-}));
+// app.use(passport.initialize());
+// app.use(passport.session());
+// passport.serializeUser((user, cb) => cb(null, user));
+// passport.deserializeUser((user, cb) => cb(null, user));
+// passport.use(new WebAppStrategy({
+// 	tenantId: "",
+// 	clientId: "",
+// 	secret: "",
+// 	oauthServerUrl: "",
+// 	redirectUri: "http://localhost:3000/appid/callback"
+// }));
 
-// Handle Login
-app.get('/appid/login', passport.authenticate(WebAppStrategy.STRATEGY_NAME, {
-	successRedirect: '/',
-	forceLogin: true
-}));
 
 // Handle callback
-app.get('/appid/callback', passport.authenticate(WebAppStrategy.STRATEGY_NAME));
-
-// Handle logout
-app.get('/appid/logout', function(req, res){
-	WebAppStrategy.logout(req);
-	res.redirect('/');
-});
+//app.get('/appid/callback', passport.authenticate(WebAppStrategy.STRATEGY_NAME));
 
 // Protect the whole app
 // app.use(passport.authenticate(WebAppStrategy.STRATEGY_NAME));
 
-// Make sure only requests from an authenticated browser session can reach /api
-app.use('/api', (req, res, next) => {
-	if (req.user){
-		next();
-	} else {
-		res.status(401).send("Unauthorized");
-	}
-});
-
-// The /api/user API used to retrieve name of a currently logged in user
-app.get('/api/user', (req, res) => {
-	// console.log(req.session[WebAppStrategy.AUTH_CONTEXT]);
-	res.json({
-		user: {
-			name: req.user.name
-		}
-	});
-});
 
 // Serve static resources
 app.use(express.static('./public'));
